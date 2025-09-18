974c15c697fe2c5cfc401c7ba5272878f8f59a7d
panhalepritiwork-intern <panhalepritiwork@gmail.com>
2025-09-18 12:15:12 +0530

    Initial boilerplate setup with React + Vite + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1> Task Tracker</h1>
+      <p>Welcome to React + Vite + TypeScript boilerplate!</p>
+    </div>
+  );
+}
+
+export default App;
